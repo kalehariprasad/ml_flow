6e3e02a7c8d8dc3448c6be71b2033bba52b5f636
Hari prasad <hariprasad9693@gmail.com>
2024-09-12 17:22:52 +0530

    registered model through code added dagshub,mlflow uri

--- register_model.py
+++ register_model.py
@@ -1,14 +1,17 @@
 from mlflow.tracking import MlflowClient
 import mlflow
-client=MlflowClient()
 import time
+import dagshub
 
+dagshub.init(repo_owner='kalehariprasad', repo_name='ml_flow', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/kalehariprasad/ml_flow.mlflow')
+
+client=MlflowClient()
 run_id='1de2a7c27a6d4378b9bb5a227dc02c59'
 model_path="mlflow-artifacts:/6e3fee7ee2e0452b8bfe35f65866b7a4/1de2a7c27a6d4378b9bb5a227dc02c59/artifacts/random_forest"
-model_uri=f"runs/{run_id}/{model_path}"
-model_name="diabetes-RF-1"
-
-result=mlflow.register_model(model_uri,model_name)
+model_name="diabetes-RF"
+model_uri = f"runs:/{run_id}/{model_path}"
+result=mlflow.register_model(model_uri=model_uri, name=model_name)
 
 time.sleep(5)
 client.update_model_version(
