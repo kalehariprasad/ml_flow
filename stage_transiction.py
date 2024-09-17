a1325f020bb98af0a72e6ad66abc31ff3c441079
Hari prasad <hariprasad9693@gmail.com>
2024-09-17 11:22:52 +0530

    stage_transiction.py&prediction_inference.py added in work flow

--- stage_transiction.py
+++ stage_transiction.py
@@ -2,9 +2,21 @@ from mlflow.tracking import MlflowClient
 import mlflow
 import time
 import dagshub
+import os
 
-dagshub.init(repo_owner='kalehariprasad', repo_name='ml_flow', mlflow=True)
-mlflow.set_tracking_uri('https://dagshub.com/kalehariprasad/ml_flow.mlflow')
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "kalehariprasad"
+repo_name = "ml_flow"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 client=MlflowClient()
 
