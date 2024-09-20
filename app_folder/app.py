4d080f11b459d473aed9cba377555f7a95f4c92f
Hari prasad <hariprasad9693@gmail.com>
2024-09-20 10:07:26 +0530

    updated app.py file

--- app_folder/app.py
+++ app_folder/app.py
@@ -4,14 +4,22 @@ import dagshub
 import mlflow
 import pandas as pd
 import streamlit as st
+import os
 
 
-dagshub.init(repo_owner='kalehariprasad', repo_name='ml_flow', mlflow=True)
-mlflow.set_tracking_uri('https://dagshub.com/kalehariprasad/ml_flow.mlflow')
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
-model_name="diabetes-RF"
-version=1
-model=mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{version}")
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "kalehariprasad"
+repo_name = "ml_flow"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # Define feature names and expected types
 feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
